68620ea8d0963da7351750649e557875ab60d636
Felipe <felipe@gmail.com>
2019-08-22 20:11:11 -0300

    Added my name

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello {
     public static void main(String args[]){
-        System.out.println("HELLO CLASS");
+        private String name = "Felipe";
+        System.out.println(name));
     }
 }
